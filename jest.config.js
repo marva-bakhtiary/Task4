99bdee70f544f6646fca07b376618ab8f2b99d87
marva bakhtiary <marva.bakhtiyari@gmail.com>
2022-04-13 13:00:58 +0430

    eslint & prettier configurations for Test

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  testMatch: ['<rootDir>/src/components/**/**/?(*.)+(spec|test).[tj]s?(x)'],
+  testEnvironment: 'jsdom',
+  preset: 'ts-jest',
+  moduleNameMapper: {
+    '\\.(css|pcss)$': 'identity-obj-proxy',
+  },
+}
